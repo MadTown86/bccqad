471b202671033c1af6fba54e681385efc78872aa
MadTown86 <grover.donlon@gmail.com>
2025-03-25 17:25:19 -0500

    Episode 12 start.  Need to finish episode 12 practice/answers.  Recorded rev. 1.

---
+++ episode12.py
@@ -0,0 +1,120 @@
+# Episode 12: Classes 1 of 2
+
+"""
+Simple Terms:
+A class is a way for the developer to package code into a reusable structure.
+
+The class itself is a blueprint from which instance objects are created.
+
+It's utility becomes more apparent when you dive deeper into object-oriented
+programming.  To give a simplified explanation of OOP, it is a way to break
+a large program down into smaller chunks that are easier to manage, as well as
+easier to delegate to different developers.
+
+Lastly, classes are great for making programs that can be easily extended and
+maintained through inheritance and polymorphism.
+"""
+
+"""
+Basic Class Creation Syntax:
+
+class ClassName(ParentClass1, ParentClass2, ...):
+    def __init__(self, attribute1, attribute2, ...):
+        self.attribute1 = attribute1
+        self.attribute2 = attribute2
+        ...
+    
+    def method1(self):
+        return something
+
+    def method2(self):
+        return something
+
+Basic Class Instantiation Syntax:
+
+instance1 = ClassName(attribute1, attribute2, ...)
+instance2 = ClassName(attribute1, attribute2, ...)
+
+Class Attribute Call Syntax:
+
+instance1.attribute1
+instance1.attribute2
+
+Class Method Call Syntax: ### Methods are just the name of functions belonging to a class
+
+instance1.method1()
+instance1.method2()
+
+One strange thing that is important is the use of 'self'.
+'self' is a reference to the instance of the class.  It is used to access
+variables that belong to the class.  It is also used to call other methods
+within the class.
+
+To simplify the explanation, 'self' is just the way Python refers to the instance
+of the class.  It is similar to 'this' in other programming languages and simply
+is a syntactical requirement.
+
+Lastly and most importantly, classes are the way you can fully customize the behavior of
+Python objects.  You will learn more about how to do this in the next episode classes 2 of 2.  
+"""
+
+# Simple Class Creation:
+class Employee:
+    pass
+
+# Instantiation:
+emp_1 = Employee()
+emp_2 = Employee()
+
+# Class With Attributes:
+class Employee:
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+
+# Instantiation:
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'User', 600)
+
+print(emp_1)
+print(dir(emp_1))
+"""
+Here you are creating two objects in memory that are instances of the Employee class.
+Each object has its own state, which is stored in attributes.
+"""
+
+# Accessing Attributes:
+print(emp_1.first)
+print(emp_1.last)
+
+# Class With Attributes and Methods:
+class Employee:
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.placeholder = None
+
+    def full_name(self):
+        return f'{self.first} {self.last}'
+    
+    def apply_raise(self):
+        self.pay = int(self.pay * 1.04)
+    
+    def alter_placeholder(self, value):
+        self.placeholder = value
+
+# Instantiation:
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'User', 600)
+
+# Updating Attributes:
+print(emp_1.pay)
+emp_1.apply_raise()
+print(emp_1.pay)
+
+# Accessing Methods:
+print(emp_1.full_name())
+print(emp_2.full_name())
+
---
+++ episode12_practice.py
@@ -0,0 +1,48 @@
+# Episode 12 Practice: Class Basics
+
+# 1. What are functions defined in a class called?
+# 2. What is the term used to describe the creation of an object from a class?
+
+# 1. Create a class called 'Person' with the following attributes:
+#     - name
+#     - age
+#     - occupation
+#     - salary
+
+# 1-A. Create methods that allow you to change all of the attributes of the class.
+
+# 1-B. Create a method that prints out all of the attributes of the class.
+
+# 1-C. Create a method that increases the salary of the person by 10%.
+
+# 1-D. Create two instances of this class and test all of the methods.
+
+#2. Stretch Problem:
+"""
+Create a dog class with the following attributes:
+    - name
+    - age
+    - breed
+    - color
+    - owner - class instance of 'Owner'
+
+Create a class called 'Owner' with the following attributes:
+    - person - class instance of 'Person'
+    - pets - a list of class instances of 'Dog'
+
+Create a cat class with the following attributes:
+    - name
+    - age
+    - breed
+    - color
+    - owner - class instance of 'Owner'
+
+Create a class called 'Customer' with the following attributes:
+    - owner - class instance of 'Owner'
+    - purchase_history - a list of strings
+
+Create methods that allow you to change all of the attributes of the classes above.
+
+Imagine that you have a petstore and are going to keep track of customers, pets, and owners.
+Create instances of the classes above and test all of the methods.    
+"""
