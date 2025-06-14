23146250419c38f5710a5d3f4349a2492ed5fddd
MadTown86 <grover.donlon@gmail.com>
2025-06-13 20:56:00 -0500

    Completed 2 leetcode problems. Mediam and Hard without help!

--- LEET/BCC_LEET_P4.py
+++ LEET/BCC_LEET_P4.py
@@ -50,27 +50,92 @@ class MergeTree:
             return (self._data[mid-1]+self._data[mid]) / 2
         else:
             return self._data[mid]
+        
+# Best Answer - https://leetcode.com/problems/median-of-two-sorted-arrays/solutions/4070500/99-journey-from-brute-force-to-most-optimized-three-approaches-easy-to-understand/
+
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+
+
+        n1 = len(nums1) #7
+        n2 = len(nums2) #18
+        
+        # Ensure nums1 is the smaller array for simplicity
+        if n1 > n2:
+            return self.findMedianSortedArrays(nums2, nums1)
+        
+        n = n1 + n2 # total length for odd/even calculation
+        # 25
+        left = (n1 + n2 + 1) // 2 # Calculate the left partition size 'actual' index position for median
+        # 13
+
+        low = 0
+        high = n1 # setting the high limit on the total length of the smaller array
+        
+        while low <= high:
+            mid1 = (low + high) // 2 # Calculate mid index for nums1
+            mid2 = left - mid1 # Calculate mid index for nums2
+
+            # Infinite placeholders in case end of bounds are reached
+            l1 = float('-inf')
+            l2 = float('-inf')
+            r1 = float('inf')
+            r2 = float('inf')
+            
+            # Determine values of l1, l2, r1, and r2
+            if mid1 < n1:
+                r1 = nums1[mid1]
+                #r1 is the middle value of nums1
+            if mid2 < n2:
+                r2 = nums2[mid2]
+            if mid1 - 1 >= 0:
+                l1 = nums1[mid1 - 1]
+            if mid2 - 1 >= 0:
+                l2 = nums2[mid2 - 1]
+            
+            if l1 <= r2 and l2 <= r1:
+                # The partition is correct, we found the median
+                if n % 2 == 1:
+                    return max(l1, l2)
+                else:
+                    return (max(l1, l2) + min(r1, r2)) / 2.0
+            elif l1 > r2:
+                # We are trying to include less from nums1, so we need to move left
+                # Move towards the left side of nums1
+                high = mid1 - 1
+            else:
+                # Move towards the right side of nums1
+                # We are trying to include more from nums1, so we need to move right
+                low = mid1 + 1
+        
+        return 0 # If the code reaches here, the input arrays were not sorted.
 
         
 if __name__ == "__main__":
-    M = MergeTree()
-    nums1 = [1, 45, 99, 8900, 40005]
-    nums2 = [1, 2, 4, 7, 10, 840, 8009, 999999]
-    while nums1 and nums2:
-        if nums1[0] > nums2[0]:
-            item = nums2.pop(0)
-        else:
-            item = nums1.pop(0)
-        print(item)
-        M.insert(item)
+    # My answer test
+    # M = MergeTree()
+    nums1 = [1, 45, 99, 8900, 40005, 41000, 42999]
+    nums2 = [1, 2, 4, 7, 10, 840, 8009, 999999, 1000000, 1000001, 1000002, 1000003, 1000004, 1000005, 1000006, 1000007, 1000008, 1000009]
+    # region MyAnswer
+    # while nums1 and nums2:
+    #     if nums1[0] > nums2[0]:
+    #         item = nums2.pop(0)
+    #     else:
+    #         item = nums1.pop(0)
+    #     print(item)
+    #     M.insert(item)
 
-    if nums1:
-        for item in nums1:
-            M.insert(item)
-    else:
-        for item in nums2:
-            M.insert(item)
+    # if nums1:
+    #     for item in nums1:
+    #         M.insert(item)
+    # else:
+    #     for item in nums2:
+    #         M.insert(item)
 
-    print(M._data)
-    print(M.median())
+    # print(M._data)
+    # print(M.median()
+    # endregion
+
+    S = Solution()
+    S.findMedianSortedArrays(nums1, nums2)
 
---
+++ LEET/BCC_LEET_P6.py
@@ -0,0 +1,165 @@
+# https://leetcode.com/problems/zigzag-conversion/description/
+# Difficulty: Medium
+# Name: Zigzag Conversion
+
+class Solution:
+    """
+    634ms runtime
+    34.35 MB space
+
+    Not a great solution, but at least I could complete it without looking at the solutions.
+    """
+    def convert(self, s: str, numRows: int) -> str:
+        
+        if len(s) <= numRows or numRows == 1:
+            return s
+
+        spacing = numRows - 2 # Used to calculate initial matrix dimensions
+        col_count = 0
+        col_start = len(s)
+
+        # spacing_count set-up for smaller input sizes
+
+        if numRows == 3:
+            spacing_count = 1
+        elif numRows > 3:
+            spacing_count = numRows - 2
+        else:
+            spacing_count = 0
+        
+
+        # Calculating required length for each row based on length of string input and numRows
+        flag = True
+        while col_start:
+            if flag:
+                if (col_start - numRows) >= 0:
+                    col_start -= numRows
+                    col_count += 1
+                    flag = False
+                elif -numRows < (col_start - numRows) < 0:
+                    col_start -= numRows
+                    col_count += 1
+                    break
+            else:
+                if (col_start - spacing) >= 0:
+                    col_start -= spacing
+                    col_count += spacing
+                    flag = True
+                elif -spacing < (col_start - spacing) < 0:
+                    col_count += col_start 
+                    break
+        
+        # Building an empty array to populate
+        # NOTE: You don't need to re-build it and waste memory space/time - you could just use a mathematical approach
+        res = [[[] for x in range(col_count)] for x in range(numRows)]
+        
+
+        # Matrix input counters
+        col_by_row = 0 # row per column
+        col_count = 0 # what column you are currently on
+        flag = True # Are you populating a 'full column' or a 'zag' 
+        
+        x = 0 # Counter/Index for string
+
+        # Loop for filling res matrix
+        while x < len(s):
+            if flag: 
+                # if you are populating a 'zig'
+                if col_by_row < numRows:
+                    res[col_by_row][col_count] = s[x]
+                    col_by_row += 1
+                    x += 1
+                elif col_by_row >= numRows:
+                    flag = False
+                    col_count += 1
+                    col_by_row = 0
+            else:
+                # if you are populating a 'zag'
+                if spacing_count == 0:
+                    flag = True
+                    spacing_count = numRows -2 if numRows > 3 else 1 if numRows == 3 else 0
+
+                elif 0 < spacing_count <= numRows - 2:
+                    res[spacing_count][col_count] = s[x]
+                    spacing_count -= 1
+                    col_count += 1
+                    x += 1
+
+        string_res = ''
+        for row in res:
+            for col in row:
+                string_res += col[0] if col else ''
+        
+        return string_res
+    
+class Solution2:
+    """
+    Beats 40% - Time Complexity
+    Beats 90% - Memory Complexity
+    """
+    def convert(self, s: str, numRows: int) -> str:
+        res = [[] for x in range(numRows)]
+        u = len(s)
+
+        if u <= numRows or numRows == 1:
+            return s 
+        
+        sp = 0 if numRows < 3 else numRows - 2
+        
+
+        rc = 0
+        sl = sp
+        f = True
+        x = 0
+        while x < u:
+            if f and rc < numRows:
+                res[rc].append(s[x])
+                rc += 1
+                x += 1
+            elif f and rc >= numRows:
+                rc = 0
+                f = False
+
+            elif not f and sl > 0:
+                res[sl].append(s[x])
+                sl -= 1
+                x += 1
+            
+            else:
+                sl = sp
+                f = True
+
+        return ''.join(''.join(x) for x in res)
+    
+# https://leetcode.com/problems/zigzag-conversion/solutions/6146922/two-points-to-solve-the-question/
+# Highest Upvote Python Solution
+
+class ThierSolution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        idx, d = 0, 1
+        rows = [[] for _ in range(numRows)]
+
+        for char in s:
+            rows[idx].append(char)
+            if idx == 0:
+                d = 1
+            elif idx == numRows - 1:
+                d = -1
+            idx += d
+
+        for i in range(numRows):
+            rows[i] = ''.join(rows[i])
+
+        return ''.join(rows)   
+    
+if __name__ == "__main__":
+    S = Solution2()
+    # ("PAYPALISHIRING", 3), ("PAYPALISHIRING", 4), ("A", 5), ("AB", 1), ("ABC", 2),
+    inputs = [("PAYPALISHIRING", 3), ("PAYPALISHIRING", 4), ("A", 5), ("AB", 1), ("ABC", 2), ("PAYPALISHIRING", 7)]
+
+    for question in inputs:
+        print(S.convert(*question))
+                
