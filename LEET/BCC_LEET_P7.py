0b2d1abba7e565f90b90874efd20258bfe34fa17
MadTown86 <grover.donlon@gmail.com>
2025-06-14 12:53:59 -0500

    Completed problem P7.

--- LEET/BCC_LEET_P7.py
+++ LEET/BCC_LEET_P7.py
@@ -11,12 +11,38 @@ Initial Thoughts:
 
 class Solution:
     def reverse(self, x: int) -> int:
-        pass
+        INT_MIN = -2**31
+        INT_MAX = 2**31-1
+        if INT_MIN < x < INT_MAX:
+            minus_sign = True if x < 0 else False
+            reversed = int(str(abs(x))[::-1])
+            if INT_MIN < reversed < INT_MAX:
+                if minus_sign:
+                    return reversed * -1
+                else:
+                    return reversed
+            else:
+                return 0
+        else:
+            return 0
+        
+class BestSolution:
+    def reverse(self, x: int) -> int:
+        res = 0
+        if x < 0:
+            res = int(str(x)[1:][::-1]) * -1
+        else:
+            res = int(str(x)[::-1])
+        
+        if res > 2 ** 31 - 1 or res < -2 ** 31:
+            return 0
+        
+        return res
+
 
 if __name__ == "__main__":
     S = Solution()
-    inp_ans = [(123, 321), (-123, -321), (120, 21)]
-    for pair in range(len(inp_ans)):
-        assert(S.reverse(inp_ans[pair][0]) == inp_ans[pair][1])
+    inp_ans = [(123, 321), (-123, -321), (120, 21), (1534236469, 0)]
+    print(S.reverse(inp_ans[3][0]))
 
     
---
+++ LEET/BCC_LEET_P8.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/string-to-integer-atoi/description/
+# Difficulty: Medium
+# Name: String to Integer (atoi)
+
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        pass
+
+if __name__ == "__main__":
+    S = Solution()
+    inputs = []
+    
