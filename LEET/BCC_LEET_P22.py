f403c6513f71e321596a78dc3979215ded2b44be
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2025-08-05 15:09:14 -0500

    Laptop commit - BCC_LEET_P22.py reconciliation issue.

--- LEET/BCC_LEET_P22.py
+++ LEET/BCC_LEET_P22.py
@@ -1,30 +1,30 @@
-# https://leetcode.com/problems/generate-parentheses/description/
-# Difficulty: Medium
-# Name: Generate Parentheses
+#https://leetcode.com/problems/generate-parentheses/
+# difficulty: Medium
+# Name: Generate Parenthesis
 
 from typing import List
 
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        if n == 1:
-            return ['()']
-        s = '()'*n
-        parse = [x for x in s]
         res = []
-        res.append(s)
-        front = 1
-        back = float('inf')
-        while front < back:
-            back = len(s)-front-1
-            parse[front] = '('
-            parse[back] = ')'
-            res.append(''.join(parse))
-            front += 2
-        
-        return res
+        x, y = 0, n-1
+        o, c = '(', ')'
 
-if __name__ == "__main__":
-    S = Solution()
-    inputs = [3, 1, 5, 6    ]
-    for inp in inputs:
-        print(S.generateParenthesis(inp))
+        def checker(s: str):
+            cs = []
+            x = 0
+            while x < len(s):
+                if s[x] == '(':
+                    cs.append(s[x])
+                    x += 1
+                else:
+                    if cs:
+                        cs.pop()
+                        x += 1
+                    else:
+                        return False
+            return True if not cs else False
+
+        # center uniform
+        while x != y:
+            
